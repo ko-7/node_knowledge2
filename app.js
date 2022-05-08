bb114a62175d3ef8adbb7e28b8083d997f914b69
shimura <shimura21ngs@gmail.com>
2022-05-08 15:32:31 +0900

    heroku test

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ var manageRouter = require('./routes/manage');
 
 var app = express();
 
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+
+
 // express-ejs-layouts
 app.use(expressLayouts);
 
