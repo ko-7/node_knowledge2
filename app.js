2a8ce8f54f15d1e9b6cde19e3ea1322381c22b18
shimura <shimura21ngs@gmail.com>
2022-05-25 21:53:51 +0900

    fix code11 http https

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const expressLayouts = require('express-ejs-layouts');
-var enforce = require('express-sslify');　//http ⇒　httpsリダイレクトに使用
 
 var indexRouter = require('./routes/index');
 var manageRouter = require('./routes/manage');
@@ -22,11 +21,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-// http ⇒　httpsリダイレクト
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+
 
 // express-ejs-layouts
 app.use(expressLayouts);
